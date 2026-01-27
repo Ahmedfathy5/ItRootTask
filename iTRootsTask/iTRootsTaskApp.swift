c4e0376290581a4477b01580ef5595f13a057278
Ahmed Fathy <ahmedfathyios1@gmail.com>
2026-01-27 17:34:36 +0200

    Initial Commit

---
+++ iTRootsTask/iTRootsTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTRootsTaskApp.swift
+//  iTRootsTask
+//
+//  Created by Ahmed Fathi on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct iTRootsTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
